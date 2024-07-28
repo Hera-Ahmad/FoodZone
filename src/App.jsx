b551bb6a3c34f22768048fde5ed98ea06d0bab83
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-28 12:32:44 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import styled from "styled-components"
+
+const App = () => {
+  return (
+    <MainContainer>
+      <TopContainer>
+
+      </TopContainer>
+    </MainContainer>
+  )
+}
+
+export default App;
+
+const MainContainer= styled.div``;
+
+const TopContainer =styled.section``;
